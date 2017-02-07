f8386d3e5ae35d810d3f712cfb7dd275d854d7a7
Dillon Huff <dillonhuff@gmail.com>
2017-02-06 23:52:07 -0800

    Initial commit

---
+++ src/parser.cpp
@@ -0,0 +1,5 @@
+#include "parser.h"
+
+namespace gpr {
+
+}
---
+++ test/parser_tests.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+namespace gpr {
+
+  TEST_CASE("Parse comment") {
+    REQUIRE(false);
+  }
+
+}
